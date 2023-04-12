620c4987a50ce87ace7b8c465423c4ebb9982968
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-04-12 12:34:48 +0530

    Fixed arrays.js, arrays-test passed

--- src/arrays.js
+++ src/arrays.js
@@ -1,20 +1,30 @@
+const isEven = function(num) {
+  return num % 2 === 0;
+}
+
+const isOdd = function(num) {
+  return !(isEven(num));
+}
+
 const selectEvenNumbers = function(numbers) {
-	const evenNumbers = [];
-	for(const number of numbers) {
-		if (isEven(number)) {
-			evenNumbers.push(number);
-		}
-	}
-	return evenNumbers;
+  const evenNumbers = [];
+  for(const number of numbers) {
+    if (isEven(number)) {
+      evenNumbers.push(number);
+    }
+  }
+  return evenNumbers;
 }
 
 const selectOddNumbers = function(numbers) {
-	const evenNumbers = [];
-	for(const number of numbers) {
-		if (isOdd(number)) {
-			evenNumbers.push(number);
-		}
-	}
-	return evenNumbers;
+  const evenNumbers = [];
+  for(const number of numbers) {
+    if (isOdd(number)) {
+      evenNumbers.push(number);
+    }
+  }
+  return evenNumbers;
 }
 
+exports.selectEvenNumbers = selectEvenNumbers;
+exports.selectOddNumbers = selectOddNumbers;
--- test/arrays-test.js
+++ test/arrays-test.js
@@ -1,6 +1,9 @@
+const test = require("../lib/testing.js")
+const arrays = require("../src/arrays.js")
+const selectEvenNumbers = arrays.selectEvenNumbers
 
 const testSelectEvenNumbers = function() {
-	test.assertListsEqual([2, 4, 6], selectEvenNumbers([1,2,3,4,5,6,7]), "Select even numbers");
+  test.assertListsEqual([2, 4, 6], selectEvenNumbers([1,2,3,4,5,6,7]), "Select even numbers");
 }
 
 testSelectEvenNumbers();
